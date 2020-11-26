c43c87a0df397bb90400565eac60f126f1e410ae
YDongY <1140068769@qq.com>
2020-11-26 12:50:49 +0800

    fix:update cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/C_Programming_Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ydongy/Code/C-Programming-Tutorial/05_数组/07_字符数组.c" "/home/ydongy/Code/C-Programming-Tutorial/cmake-build-debug/CMakeFiles/C_Programming_Tutorial.dir/05_数组/07_字符数组.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
